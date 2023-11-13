5c8dec2cdf73178850617ca4726be0bc9f9482fb
jackcat13 <christophehenry11@gmail.com>
2023-11-13 11:10:39 +0100

    Create new Remotion video

---
+++ src/Root.tsx
@@ -0,0 +1,31 @@
+import {Composition} from 'remotion';
+import {HelloWorld, myCompSchema} from './HelloWorld';
+
+// Each <Composition> is an entry in the sidebar!
+
+export const RemotionRoot: React.FC = () => {
+	return (
+		<>
+			<Composition
+				// You can take the "id" to render a video:
+				// npx remotion render src/index.ts <id> out/video.mp4
+				id="HelloWorld"
+				component={HelloWorld}
+				durationInFrames={300}
+				fps={60}
+				width={1920}
+				height={1080}
+				// You can override these props for each render:
+				// https://www.remotion.dev/docs/parametrized-rendering
+				schema={myCompSchema}
+				defaultProps={{
+					titleText: 'Welcome to Remotion',
+					titleColor: '#eeeeee',
+					logoColor1: '#91EAE4',
+					logoColor2: '#86A8E7',
+				}}
+			/>
+			
+		</>
+	);
+};
